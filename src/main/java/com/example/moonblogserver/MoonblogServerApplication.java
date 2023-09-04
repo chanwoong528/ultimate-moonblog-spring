5bd3456cefc1efa1b5bf3403060b5c124fe7cf18
moon <cksdnd004@naver.com>
2023-09-04 21:12:57 +0900

    first commit

---
+++ src/main/java/com/example/moonblogserver/MoonblogServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.moonblogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonblogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoonblogServerApplication.class, args);
+    }
+
+}
