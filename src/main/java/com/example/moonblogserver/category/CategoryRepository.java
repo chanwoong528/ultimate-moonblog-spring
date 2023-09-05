f5755dca03ff1be06abbf0439365932aa95087a6
문찬웅 <mooncw@hankookilbo.com>
2023-09-05 18:01:52 +0900

    cate finished

--- src/main/java/com/example/moonblogserver/category/CategoryRepository.java
+++ src/main/java/com/example/moonblogserver/category/CategoryRepository.java
@@ -1,4 +1,13 @@
 package com.example.moonblogserver.category;
 
-public interface CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findById(UUID id);
 }
--- src/main/java/com/example/moonblogserver/category/CategoryService.java
+++ src/main/java/com/example/moonblogserver/category/CategoryService.java
@@ -1,4 +1,43 @@
 package com.example.moonblogserver.category;
 
+import com.example.moonblogserver.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> allCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category oneCategory(UUID id) {
+        return categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+    }
+
+    public Category createdOneCategory(String label, String description) {
+        Category newCategory = new Category(label, description);
+        return categoryRepository.save(newCategory);
+    }
+
+    public Category updateOneCategory(UUID id, String label, String description) {
+        Category tobeUpdatedCategory = categoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+        if (!label.isEmpty()) {
+            tobeUpdatedCategory.setLabel(label);
+        }
+        if (!description.isEmpty()) {
+            tobeUpdatedCategory.setDescription(description);
+        }
+        return categoryRepository.save(tobeUpdatedCategory);
+    }
+
+
 }
